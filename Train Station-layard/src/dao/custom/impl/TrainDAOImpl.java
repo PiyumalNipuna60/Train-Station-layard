06c40ed6be13f4933fabbe10724c3cb9fc72edcf
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-05 00:06:41 +0530

    Add TrainDaoImpl full method

--- Train Station-layard/src/dao/custom/impl/TrainDAOImpl.java
+++ Train Station-layard/src/dao/custom/impl/TrainDAOImpl.java
@@ -11,32 +11,72 @@ import java.util.ArrayList;
 public class TrainDAOImpl implements TrainDAO {
     @Override
     public ArrayList<TrainDTO> getAll() throws SQLException, ClassNotFoundException {
+        ArrayList<TrainDTO> AllTrain = new ArrayList<>();
+        ResultSet rst = SqlUtil.executeQuery("Select * from train");
 
+        while (rst.next()){
+            AllTrain.add(
+                    new TrainDTO(
+                            rst.getString(1),
+                            rst.getString(2),
+                            rst.getString(3),
+                            rst.getString(4),
+                            rst.getString(5),
+                            rst.getString(6)
+                            ));
+        }
+        return AllTrain;
     }
 
     @Override
     public boolean Save(TrainDTO dto) throws SQLException, ClassNotFoundException {
-        return false;
+        return SqlUtil.executeUpdate("insert into train Values(?,?,?,?,?,?)",
+                dto.getTrainId(),
+                dto.getTrainName(),
+                dto.getStartTime(),
+                dto.getEndTime(),
+                dto.getTrainFrom(),
+                dto.getTrainTo()
+        );
     }
 
     @Override
     public boolean update(TrainDTO dto) throws SQLException, ClassNotFoundException {
-        return false;
+        return SqlUtil.executeUpdate("update train set trainName=?,startTime=?,EndTime=?,trainFrom=?,trainTo=? where trainId=?",
+                dto.getTrainId(),
+                dto.getTrainName(),
+                dto.getStartTime(),
+                dto.getEndTime(),
+                dto.getTrainFrom(),
+                dto.getTrainTo()
+                );
     }
 
     @Override
     public boolean delete(String s) throws SQLException, ClassNotFoundException {
-        return false;
+        return SqlUtil.executeUpdate("delete from train where trainId=?",s);
     }
 
     @Override
     public TrainDTO search(String s) throws SQLException, ClassNotFoundException {
+        ResultSet rst = SqlUtil.executeQuery("Select * from train where trainId=?", s);
+        if (rst.next()){
+            return new TrainDTO(
+                    rst.getString(1),
+                    rst.getString(2),
+                    rst.getString(3),
+                    rst.getString(4),
+                    rst.getString(5),
+                    rst.getString(6)
+            );
+        }
         return null;
     }
 
     @Override
     public boolean exist(String s) throws SQLException, ClassNotFoundException {
-        return false;
+        ResultSet resultSet = SqlUtil.executeQuery("Select trainId from train where trainId=?", s);
+        return resultSet.next();
     }
 
     @Override
