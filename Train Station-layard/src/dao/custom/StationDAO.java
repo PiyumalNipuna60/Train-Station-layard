52d4ee10e6343ce648b9270af52c7300976123e8
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-05 14:13:47 +0530

    Add Clss Content

---
+++ Train Station-layard/src/dao/custom/StationDAO.java
@@ -0,0 +1,7 @@
+package dao.custom;
+
+import dao.CrudDAO;
+import model.StationDTO;
+
+public interface StationDAO extends CrudDAO<StationDTO,String> {
+}
---
+++ Train Station-layard/src/model/StationDTO.java
@@ -0,0 +1,50 @@
+package model;
+
+public class StationDTO {
+
+    String id;
+    String name;
+    String contact;
+
+    public StationDTO() {
+    }
+
+    public StationDTO(String id, String name, String contact) {
+        this.id = id;
+        this.name = name;
+        this.contact = contact;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    @Override
+    public String toString() {
+        return "Station{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", contact='" + contact + '\'' +
+                '}';
+    }
+}
