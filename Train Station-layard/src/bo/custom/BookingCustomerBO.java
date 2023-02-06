decc1b7baad7d24f858c22a0fa8a7393aeb1f1a4
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-06 21:31:59 +0530

    Add BO class method

--- Train Station-layard/src/bo/custom/BookingCustomerBO.java
+++ Train Station-layard/src/bo/custom/BookingCustomerBO.java
@@ -7,4 +7,15 @@ import java.util.ArrayList;
 
 public interface BookingCustomerBO{
 
-  }
+    public ArrayList<BookingCustomerDTO> getAllCustomerBooking() throws SQLException, ClassNotFoundException;
+
+    public boolean saveCustomerBooking(BookingCustomerDTO dto) throws SQLException, ClassNotFoundException;
+
+    public boolean updateCustomerBooking(BookingCustomerDTO dto) throws SQLException, ClassNotFoundException;
+
+    public boolean existCustomerBooking(String id) throws SQLException, ClassNotFoundException;
+
+    public boolean deleteCustomerBooking(String id) throws SQLException, ClassNotFoundException ;
+
+    public String generateCustomerBookingNewId() throws SQLException, ClassNotFoundException;
+}
