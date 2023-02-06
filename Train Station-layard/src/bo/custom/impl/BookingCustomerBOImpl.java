08dd4e53aaf3293cbc2a4ea85b8b233d3119d147
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-06 21:33:34 +0530

    Add BookingCustomerBOImpl method

--- Train Station-layard/src/bo/custom/impl/BookingCustomerBOImpl.java
+++ Train Station-layard/src/bo/custom/impl/BookingCustomerBOImpl.java
@@ -1,7 +1,43 @@
 package bo.custom.impl;
 
 import bo.custom.BookingCustomerBO;
+import dao.custom.impl.BookingCustomerDAOImpl;
+import model.BookingCustomerDTO;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class BookingCustomerBOImpl implements BookingCustomerBO {
+    BookingCustomerDAOImpl bookingCustomerDAO = new BookingCustomerDAOImpl();
 
+    @Override
+    public ArrayList<BookingCustomerDTO> getAllCustomerBooking() throws SQLException, ClassNotFoundException {
+
+        return null;
+    }
+
+    @Override
+    public boolean saveCustomerBooking(BookingCustomerDTO dto) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean updateCustomerBooking(BookingCustomerDTO dto) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean existCustomerBooking(String id) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean deleteCustomerBooking(String id) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public String generateCustomerBookingNewId() throws SQLException, ClassNotFoundException {
+        return null;
+    }
 }
